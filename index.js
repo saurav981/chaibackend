24617c652f23097ea37902d3d0852bc1a1b4c6a8
saurav981 <souravhalder981@gmail.com>
2024-08-28 17:31:18 +0530

    Fixed some bug

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get("/about", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+  console.log(`Example app listening on port ${port}`);
 });
